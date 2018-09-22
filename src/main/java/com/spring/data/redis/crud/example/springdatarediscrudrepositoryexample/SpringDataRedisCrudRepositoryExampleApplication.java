15eb101cc6a3f662d297462f07f343c1dbfea0a4
abhimanyu <abhimanyu081@outlook.com>
2018-09-22 19:18:36 +0530

    intial commit

---
+++ src/main/java/com/spring/data/redis/crud/example/springdatarediscrudrepositoryexample/SpringDataRedisCrudRepositoryExampleApplication.java
@@ -0,0 +1,12 @@
+package com.spring.data.redis.crud.example.springdatarediscrudrepositoryexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRedisCrudRepositoryExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRedisCrudRepositoryExampleApplication.class, args);
+	}
+}
